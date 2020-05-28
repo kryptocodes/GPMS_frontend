5289009df433b7dfbc1b20ea42b5f0480e321f3e
kryptocodes <srivatsantb@gmail.com>
2020-05-28 22:56:07 +0530

    Added entry_check
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/security/entry_check.js
+++ src/security/entry_check.js
@@ -1,55 +1,148 @@
 import React,{useState} from 'react'
-import QrReader from 'react-qr-scanner'
+import QrReader from 'react-qr-reader'
 import Base from '../Home/base'
-import { getPass } from '../auth/pass'
+import { getPass, updatePass } from '../auth/pass'
+import { isAuthenticated } from '../auth'
+
+
 
 const Entry_check = () => {
 
     const [values,setValues] = useState({
-        result:1,
-        delay:500,
-        store:""
+        showQR:true,
+        showPass:false,
+        showError:false,
+        showRetry:false,
+        name:"",
+        passId:"",
+        pass_type:"",
+        exp_arr_time:"",
+        exp_dep_time:"",
+        from_date:"",
+        to_date:"",
+        curTime : new Date().toLocaleString()
     })
 
-    const {result,delay} = values
+    const date = new Date()
+
+    const {user,token} = isAuthenticated()
+
+    const {showQR,showPass,showRetry,showError,name,passId,pass_type,exp_arr_time,exp_dep_time,from_date,to_date,curTime} = values
 
     const handleScan = value => {
+        console.log(value)
         if(value != null){
         getPass(value)
         .then(data => {
-            console.log(data)
             if(data.error){
                 console.log(data.error)
+                setValues({showQR:false,showError:data.error})
             } else{
                 console.log(data)
+                setValues({
+                    ...values,
+                    showQR:false,
+                    showPass:true,
+                    name:data.info.name,
+                    passId:data.passId,
+                    pass_type:data.pass_type,
+                    exp_arr_time:data.exp_arr_time,
+                    exp_dep_time:data.exp_dep_time,
+                    from_date:data.from_date,
+                    to_date:data.to_date
+                })
+                console.log(values)
             }
         })
 
     }
     }
 
+    const NoPass = () => (
+        <div>
+        <h1 className="display-5">{showError}</h1>
+        {Retry()}
+        </div>
+    )
+
+    const Retry = () => (
+        <div>
+        <button onClick={() => {setValues({showQR:true,showError:false})}} 
+          className="btn btn-lg btn-warning">New Scan</button>
+        </div>
+    )
+
+    const PassInfo = () => (
+            <React.Fragment>
+            <div className="jumbotron-fluid">
+                <ul className="list-group">
+                <div className="card my-4 p-4">
+                        <h4 className="card-header row m-0 bg-dark text-white bd-highlight">
+                            <div className="col-md-8">
+                            <span><span className="badge badge-success flex-grow-1 mr-2">Name:</span>{name}</span>
+                            </div>
+                            <div className="col-md-4">
+                            <span><span className="badge badge-success mr-2">Pass type</span>{pass_type}</span>
+                            </div>
+                         </h4>
+            <div className="d-flex flex-column">
+            <li className="list-group-item flex-grow-1">
+                <span className="badge badge-success mr-2">Expected leaving time:</span>{exp_dep_time}
+            </li>
+            <li className="list-group-item flex-grow-1">
+                <span className="badge badge-success mr-2">Expected arrival time:</span>{exp_arr_time}
+            </li>
+            </div>
+            {(pass_type === "HomePass") && (
+            <div className="d-flex flex-row">
+            <li className="list-group-item flex-grow-1">
+                <span className="badge badge-success mr-2">From:</span>{from_date}
+            </li>
+            <li className="list-group-item flex-grow-1">
+                <span className="badge badge-success mr-2">To:</span>{to_date}
+            </li>
+            </div>
+            )}
+            <li className="list-group-item flex-grow-1">
+                <span className="badge badge-success mr-2">Entry Date and Time:</span>{curTime}
+            </li>
+            </div>
+            </ul> 
+            {Retry()}
+            </div>
+            </React.Fragment>         
+        )
+
     const handleError = err => {
         console.log(err)
     }
-    const previewStyle =  {
-        height: 240,
-        width: 240
-      }
+
+
+    
 
     return (
         <Base title="Entry" className="container p-2">
         <div className="row m-0">
-        <div className="col-md-2">
-        {(result === 1) && 
+        {showQR && (
+        <div className="container col-md-5">
         <QrReader
           delay={20}
-          style={{width: '90vw',maxWidth: "400px"}}
+          className="w-100"
           onError={handleError}
           onScan={handleScan}
-          />}
-        <p>{result}</p>
+          />
+          </div>)}
+        {showError && (
+            <div className="col-md-5 offset-sm-2">
+            {NoPass()}
+            </div>
+        )}
+        {showPass && (
+            <div className="col-md-8 offset-sm-2">
+            {PassInfo()}
+            </div>
+            )}
       </div>
-        </div>
         </Base>
     )
 }
